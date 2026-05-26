8e9df094a707d5565e8806b2840b0d77e537115f
alexbart <alexbart695@gmail.com>
2026-05-26 21:28:52 +0300

    Fix: Cors errors

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use(cookieParser());
